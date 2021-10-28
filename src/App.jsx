6b4e8328a95799026b72f5322f2a4edf2da4ed4e
Tasnimul Hasan <thtanim2005@gmail.com>
2021-10-29 00:08:35 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <Button variant="primary">Hello</Button>
+    </div>
+  )
+}
+
+export default App
